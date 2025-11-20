a90124b0962672ebb235d998fca7594bc0424563
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-11-20 15:19:27 +0700

    fix: metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,33 +7,33 @@
 </script>
 
 <svelte:head>
-	<title>Rinkai Studio – Empowering your business with Rinkai</title>
+	<title>Rinkai Industries – Make life easier</title>
 	<meta
 		name="description"
-		content="Rinkai Studio is dedicated to providing innovative solutions for your business needs."
+		content="Rinkai Industries is dedicated to providing innovative solutions for your business needs."
 	/>
 	<meta name="robots" content="index, follow" />
-	<link rel="canonical" href="https://beta.rinkai.co" />
+	<link rel="canonical" href="https://rinkai.co" />
 
 	<!-- OpenGraph -->
-	<meta property="og:title" content="Rinkai Studio – Empowering your business with Rinkai" />
+	<meta property="og:title" content="Rinkai Industries – Rinkai Industries – Make life easier" />
 	<meta
 		property="og:description"
-		content="Rinkai Studio is dedicated to providing innovative solutions for your business needs."
+		content="Rinkai Industries is dedicated to providing innovative solutions for your business needs."
 	/>
-	<meta property="og:url" content="https://beta.rinkai.co" />
+	<meta property="og:url" content="https://rinkai.co" />
 	<meta property="og:type" content="website" />
-	<meta property="og:image" content="https://beta.rinkai.co/og-img.webp" />
+	<meta property="og:image" content="https://rinkai.co/og-img.webp" />
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
 	<meta property="og:image:alt" content="Rinkai Studio" />
 
 	<!-- Twitter -->
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content="Rinkai Studio – Empowering your business with Rinkai" />
+	<meta name="twitter:title" content="Rinkai Industries – Rinkai Industries – Make life easier" />
 	<meta
 		name="twitter:description"
-		content="Rinkai Studio is dedicated to providing innovative solutions for your business needs."
+		content="Rinkai Industries is dedicated to providing innovative solutions for your business needs."
 	/>
 	<meta name="twitter:image" content="https://beta.rinkai.co/og-img.webp" />
 
